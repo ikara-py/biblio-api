e8342cb2914ee057d572434b54904a5fe667d04e
ikara-py <ali16kara@gmail.com>
2026-03-09 13:15:38 +0000

    Refactor : enhance resource handling and response structure

--- app/Http/Controllers/Api/CategoryController.php
+++ app/Http/Controllers/Api/CategoryController.php
@@ -3,40 +3,70 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Models\Category;
 use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\JsonResponse;
 
 class CategoryController extends Controller
 {
     /**
      * Display a listing of the resource.
      */
-    public function index()
+    public function index() : JsonResponse
     {
-        //
+        $categories = Category::withCount('books')->get();
+        return response()->json([
+            'status' => 'success',
+            'data' => $categories
+        ]);
     }
 
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
+    public function store(Request $request) :JsonResponse
     {
-        //
+        $validated = $request->validate([
+            'name' => 'required|string|max:255',
+            'description' => 'nullable|string'
+        ]);
+        $category = Category::create($validated);
+        return response()->json([
+            'status' => 'success',
+            'message' => 'categorie creee avec succes',
+            'data' => $category
+        ], 201);
     }
 
     /**
      * Display the specified resource.
      */
-    public function show(string $id)
+    public function show(Category $category)
     {
-        //
+        $category->load('books');
+        return response()->json([
+            'status' => 'success',
+            'data' => $category
+        ]);
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, string $id)
+    public function update(Request $request, Category $category)
     {
-        //
+        $validated = $request->validate([
+            'name' => 'sometimes|required|string|max:255',
+            'description' => 'nullable|string'
+        ]);
+
+        $category->update($validated);
+
+        return response()->json([
+            'status' => 'success',
+            'message' => 'Catégorie mise à jour',
+            'data' => $category
+        ]);
     }
 
     /**
--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,16 @@
 <?php
 
+use App\Http\Controllers\BookController;
+use App\Http\Controllers\CategoryController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+
+Route::apiResource('categories', CategoryController::class);
+Route::apiResource('books', BookController::class);
+
+Route::get('categories/{category}/books', [CategoryController::class, 'books']);
